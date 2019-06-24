47c3640ba7b45b07510d370546c8a527264924ab
Tarek-Zaher <Tarek21Zaher@gmail.com>
2019-06-24 18:47:59 -0500

    add contact page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const nodemailer = require('nodemailer');
 
 const app = express();
 
@@ -17,6 +18,43 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.post("/contact", (req, res) => {
+  let name = req.body.name;
+  let email = req.body.email;
+  let phone = req.body.phone;
+  let company = req.body.company;
+  let message = req.body.message;
+
+  let mailOpts, smtpTrans;
+  smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS
+    }
+  });
+  mailOpts = {
+    from: name + ' &lt;' + email + '&gt;',
+    to: process.env.GMAIL_USER,
+    subject: 'New Customer Response from lufkinswebdesigner.com ',
+    html: `<p>${name} (${email}) from the company ${company} sent the following message:</p> <p>${message}</p> <p>From,</p> <p>${name}</p> <p>${email}</p> <p>${phone}</p> <p>${company}</p>`
+  };
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+    if (error) {
+      res.render('send-failure');
+    }
+    else {
+      res.render('send-success');
+    }
+  });
 });
 
 
